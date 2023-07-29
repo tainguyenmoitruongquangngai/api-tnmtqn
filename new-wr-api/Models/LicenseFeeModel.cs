dbcf62c7c70dd60317f89b21f849262939cb1ce3
githubnguyendang <73978541+githubnguyendang@users.noreply.github.com>
2023-07-29 09:41:45 +0700

    Create LicenseFeeModel.cs

---
+++ new-wr-api/Models/LicenseFeeModel.cs
@@ -0,0 +1,15 @@
+﻿namespace new_wr_api.Models
+{
+    public class LicenseFeeModel
+    {
+        public int Id { get; set; }
+        public int ChildrenId { get; set; }
+        public string? LicenseFeeNumber { get; set; }
+        public DateTime? SignDate { get; set; }
+        public int LicensingAuthorities { get; set; }
+        public double TotalMoney { get; set; }
+        public string? FilePDF { get; set; }
+        public string? Description { get; set; }
+        public List<LicenseFeeModel>? SupplementLicenseFee { get; set; }
+    }
+}
