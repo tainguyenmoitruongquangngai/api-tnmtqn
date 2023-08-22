69f0821e066ce3975c96f11f63d8a22e4cba799e
githubnguyendang <73978541+githubnguyendang@users.noreply.github.com>
2023-08-22 10:34:32 +0700

    Update migrations

---
+++ new-wr-api/Migrations/20230822031450_RenameLicense_ParentIdToChildId.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace new_wr_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameLicense_ParentIdToChildId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ParentId",
+                table: "Licenses",
+                newName: "ChildId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ChildId",
+                table: "Licenses",
+                newName: "ParentId");
+        }
+    }
+}
