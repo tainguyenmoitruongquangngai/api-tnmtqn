09f7933979954d9c42d08829cf4137827f370602
githubnguyendang <73978541+githubnguyendang@users.noreply.github.com>
2023-08-24 14:51:04 +0700

    #

---
+++ new-wr-api/Migrations/20230824074006_UpdateConstructionTypes.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace new_wr_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateConstructionTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ParentId",
+                table: "ConstructionTypes");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Parent",
+                table: "ConstructionTypes",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Parent",
+                table: "ConstructionTypes");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ParentId",
+                table: "ConstructionTypes",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
