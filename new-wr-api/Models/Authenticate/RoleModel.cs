c8741db95cea5efad40599f0b044675a2185adce
NT.Dang.20 <73978541+githubnguyendang@users.noreply.github.com>
2024-01-29 16:50:49 +0700

    Update RoleModel.cs

--- new-wr-api/Models/Authenticate/RoleModel.cs
+++ new-wr-api/Models/Authenticate/RoleModel.cs
@@ -2,7 +2,7 @@
 {
     public class RoleModel
     {
-        public string Id { get; set; } = string.Empty;
+        public string? Id { get; set; } = string.Empty;
         public string Name { get; set; } = string.Empty;
         public bool IsDefault { get; set; } = false;
         public List<DashboardModel>? Dashboards { get; set; }
